83ef2380459af783e0f37c0e2124a403a84452c0
zangwoo Jeon <create7827@naver.com>
2025-10-30 05:39:33 +0900

    [setup] 스프링 초기 세팅

---
+++ AISA/src/main/java/com/AISA/AISA/AisaApplication.java
@@ -0,0 +1,13 @@
+package com.AISA.AISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisaApplication.class, args);
+	}
+
+}
---
+++ AISA/src/test/java/com/AISA/AISA/AisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AISA.AISA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
