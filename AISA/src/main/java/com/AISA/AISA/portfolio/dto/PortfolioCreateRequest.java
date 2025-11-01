72fdd95b0138e3cdd0580e76cdada68d8d9a809f
zangwoo Jeon <create7827@naver.com>
2025-11-02 08:55:47 +0900

    feat : 포트폴리오 추가 기능 제작

---
+++ AISA/src/main/java/com/AISA/AISA/portfolio/dto/PortfolioCreateRequest.java
@@ -0,0 +1,13 @@
+package com.AISA.AISA.portfolio.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class PortfolioCreateRequest {
+    private UUID memberId;
+    private String portName;
+}
