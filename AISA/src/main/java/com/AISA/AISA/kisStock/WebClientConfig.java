6d8c42268c0eda7d2f22b668e2e2ca8b5d3b0cdc
zangwoo Jeon <create7827@naver.com>
2025-11-13 02:11:41 +0900

    feat : KIS토큰 발급 추가

---
+++ AISA/src/main/java/com/AISA/AISA/kisStock/WebClientConfig.java
@@ -0,0 +1,13 @@
+package com.AISA.AISA.kisStock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://openapi.koreainvestment.com:9443").build();
+    }
+}
